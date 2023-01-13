1409ec040057b462a8f18186a64d67cdf9979021
jum0624 <jum0624@naver.com>
2023-01-13 10:12:20 +0900

    add: day1 code upload

---
+++ Programmers/src/main/java/day1/배열뒤집기.java
@@ -0,0 +1,19 @@
+package day1;
+
+import java.util.Arrays;
+
+public class 배열뒤집기 {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        for (int i = 0, j = num_list.length-1; i < num_list.length; i++, j--) {
+            answer[j] = num_list[i];
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        배열뒤집기 sol = new 배열뒤집기();
+        int[] result = {1,2,3,4};
+        System.out.println(Arrays.toString(sol.solution(result)));
+    }
+}
---
+++ Programmers/src/main/java/day1/피자나눠먹기.java
@@ -0,0 +1,16 @@
+package day1;
+
+public class 피자나눠먹기 {
+    public int solution(int slice, int n) {
+        int answer = 1;
+        for (int i=1; i * slice < n; i++) {
+            answer = i;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        피자나눠먹기 sol = new 피자나눠먹기();
+        System.out.println(sol.solution(10,10));
+    }
+}
