4287d43ab8808cba518fba7dc0ef06210f834a5a
jum0624 <jum0624@naver.com>
2023-01-13 23:00:35 +0900

    add: day2 programmers code upload

---
+++ Programmers/src/main/java/day2/직각삼각형출력하기.java
@@ -0,0 +1,18 @@
+package day2;
+
+import java.util.Scanner;
+
+public class 직각삼각형출력하기 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; i >= j; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
