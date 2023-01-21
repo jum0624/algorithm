f76abab87a8e2d24913bf76c918de0b6bc39cf7f
jum0624 <jum0624@naver.com>
2023-01-22 02:48:23 +0900

    add: day3 programmers code upload

---
+++ Programmers/src/main/java/day3/배열의유사도.java
@@ -0,0 +1,22 @@
+package day3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class 배열의유사도 {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(s2));
+        for (String s : s1) {
+            if (arrayList.contains(s)) {
+                answer += 1;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        배열의유사도 sol = new 배열의유사도();
+        System.out.println(sol.solution(new String[]{"a", "b", "c"}, new String[]{"com", "b", "d", "p", "c"}));
+    }
+}
