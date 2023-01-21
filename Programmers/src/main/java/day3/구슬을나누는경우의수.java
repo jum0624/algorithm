1c55a68f9e9d15b4c8ba18b62725db1a422a331e
jum0624 <jum0624@naver.com>
2023-01-22 01:46:14 +0900

    fix: day3 구슬을 나누는 경우의 수 코드 수정

--- Programmers/src/main/java/day3/구슬을나누는경우의수.java
+++ Programmers/src/main/java/day3/구슬을나누는경우의수.java
@@ -2,20 +2,19 @@ package day3;
 
 public class 구슬을나누는경우의수 {
     public long solution(int balls, int share) {
-        float answer = 1;
-        float k = balls;
-        float j = share;
+        long answer = 1;
+        long k = balls;
+        long j = 1;
         for (int i = 1; i <= share; i++) {
-            answer *= k;
+            answer = answer * k / j;
             k -= 1;
-            answer /= j;
-            j = j - 1;
+            j += 1;
         }
-        return (long)answer;
+        return answer;
     }
 
     public static void main(String[] args) {
         구슬을나누는경우의수 sol = new 구슬을나누는경우의수();
-        System.out.println(sol.solution(6,2));
+        System.out.println(sol.solution(5,3));
     }
 }
