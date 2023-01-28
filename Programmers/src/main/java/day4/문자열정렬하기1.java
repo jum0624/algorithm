424de07d89717a886ad0f59ed2fe52921f9eb494
jum0624 <jum0624@naver.com>
2023-01-29 04:19:43 +0900

    add: day4 programmers code upload

--- Programmers/src/main/java/day4/문자열정렬하기1.java
+++ Programmers/src/main/java/day4/문자열정렬하기1.java
@@ -1,4 +1,25 @@
 package day4;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 public class 문자열정렬하기1 {
+    public ArrayList<Integer> solution(String my_string) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            int ascii = c;
+            if (ascii >= 48 && ascii <= 57) {
+                int num = c - 48;
+                result.add(num);
+            }
+        }
+        result.sort(Comparator.naturalOrder());
+        return result;
+    }
+
+    public static void main(String[] args) {
+        문자열정렬하기1 sol = new 문자열정렬하기1();
+        System.out.println(sol.solution("abcde0"));
+    }
 }
