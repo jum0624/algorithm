94fa14a234ac1f83d8431b6f28238e28bbd6daff
jum0624 <jum0624@naver.com>
2023-02-19 23:34:46 +0900

    add: day7 programmers code upload

---
+++ Programmers/src/main/java/day7/숨어있는숫자의덧셈2.java
@@ -0,0 +1,23 @@
+package day7;
+
+public class 숨어있는숫자의덧셈2 {
+    public int solution(String my_string) {
+        int answer = 0;
+        String[] split = my_string.split("[a-zA-Z]");
+        for (String s : split
+        ) {
+            try {
+                answer += Integer.parseInt(s);
+            } catch (NumberFormatException e) {
+                continue;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        숨어있는숫자의덧셈2 sol
+                = new 숨어있는숫자의덧셈2();
+        System.out.println(sol.solution("aAb1B2cC34oOp"));
+    }
+}
