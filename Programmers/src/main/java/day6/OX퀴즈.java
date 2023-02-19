7b889335a3ae6761208b5987e5581882543f404e
jum0624 <jum0624@naver.com>
2023-02-19 13:58:00 +0900

    add: day6 programmers code upload

--- Programmers/src/main/java/day6/OX퀴즈.java
+++ Programmers/src/main/java/day6/OX퀴즈.java
@@ -1,20 +1,43 @@
 package day6;
 
 public class OX퀴즈 {
-    public int solution(int num, int k) {
-        int answer = 0;
+    public String[] solution(String[] quiz) {
+        String[] answer = new String[quiz.length];
+        int x = 0;
+        int y = 0;
+        int z = 0;
+        int result = 0;
+        int i = 0;
 
-        String numStr = "-" + num;
-        // System.out.println(numStr);
-        String kStr = String.valueOf(k);
-
-        answer = numStr.indexOf(kStr);
+        for (String s : quiz
+        ) {
+            String[] split = s.split(" ");
+            x = Integer.parseInt(split[0]);
+            y = Integer.parseInt(split[2]);
+            z = Integer.parseInt(split[4]);
+            if (split[1].equals("+")) {
+                result = x + y;
+            } else if (split[1].equals("-")) {
+                result = x - y;
+            }
+            if (result == z) {
+                answer[i] = "O";
+            } else {
+                answer[i] = "X";
+            }
+            i += 1;
+        }
 
         return answer;
     }
 
     public static void main(String[] args) {
         OX퀴즈 sol = new OX퀴즈();
-        System.out.println(sol.solution(123456, 6));
+        String[] solution = sol.solution(new String[]{"3 - 4 = -3", "5 + 6 = 11"});
+        for (String s : solution
+        ) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
     }
 }
