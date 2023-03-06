fc9d7a1afecb9b3f11921bdca40b1a8d09221433
jum0624 <jum0624@naver.com>
2023-03-06 15:15:51 +0900

    add: day9 programmers code upload

---
+++ Programmers/src/main/java/day9/음양더하기.java
@@ -0,0 +1,19 @@
+package day9;
+
+public class 음양더하기 {
+
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        int k = 0;
+        for (int i = 0; i < absolutes.length; i++) {
+            if (signs[i] == true) {
+                k = absolutes[i];
+            } else if (signs[i] == false) {
+                k = -absolutes[i];
+            }
+            answer += k;
+        }
+        return answer;
+    }
+
+}
---
+++ Programmers/src/main/java/day9/하샤드수.java
@@ -0,0 +1,23 @@
+package day9;
+
+public class 하샤드수 {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int t = 0;
+        int k = x;
+        int sum = 0;
+        while (k != 0) {
+            t = k % 10;
+            sum += t;
+            k /= 10;
+        }
+        if (x % sum == 0) {
+            answer = true;
+        } else {
+            answer = false;
+        }
+
+
+        return answer;
+    }
+}
