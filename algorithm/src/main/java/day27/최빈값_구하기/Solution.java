3c7512719b3e00938aa4f94fd5133e69c6644bee
jum0624 <jum0624@naver.com>
2023-10-05 19:07:12 +0900

    add: day27 문제 풀이

---
+++ algorithm/src/main/java/day27/최빈값_구하기/Solution.java
@@ -0,0 +1,32 @@
+package day27.최빈값_구하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+    static int[] count;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int test_case = 1; test_case <= T; test_case++) {
+            count = new int[101];
+            int t = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int k = 0; k < 1000; k++) {
+                int score = Integer.parseInt(st.nextToken());
+                count[score] += 1;
+            }
+            int max_num = 0;
+            int max_index = 0;
+            for (int i = 0; i < count.length; i++) {
+                if (max_num <= count[i]) {
+                    max_num = count[i];
+                    max_index = i;
+                }
+            }
+            System.out.println("#" + test_case + " " + max_index);
+        }
+    }
+}
---
+++ algorithm/src/main/java/day27/피로도/Main.java
@@ -0,0 +1,39 @@
+package day27.피로도;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());;
+        int b = Integer.parseInt(st.nextToken());;
+        int c = Integer.parseInt(st.nextToken());;
+        int m = Integer.parseInt(st.nextToken());;
+        int energy = 0;
+        int work_amount = 0;
+        int hour = 0;
+        while (hour != 24) {
+            if (m < a) {
+                work_amount = 0;
+                break;
+            }
+            if (m >= energy + a) {
+                energy += a;
+                work_amount += b;
+            }
+            else {
+                energy -= c;
+                if (energy < 0) {
+                    energy = 0;
+                }
+            }
+            hour += 1;
+
+        }
+        System.out.println(work_amount);
+    }
+}
