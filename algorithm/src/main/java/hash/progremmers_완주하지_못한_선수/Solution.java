99211c51511c77ea82e9e742ce43cdaa2b642aec
jum0624 <jum0624@naver.com>
2024-04-07 18:58:30 +0900

    add: hash 문제 풀이

--- algorithm/src/main/java/hash/progremmers_완주하지_못한_선수/Solution.java
+++ algorithm/src/main/java/hash/progremmers_완주하지_못한_선수/Solution.java
@@ -1,2 +1,26 @@
-package hash.progremmers_완주하지_못한_선수;public class Solution {
+package hash.progremmers_완주하지_못한_선수;
+
+import java.util.HashMap;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String name: participant) {
+            if(!map.containsKey(name)) {
+                map.put(name, 1);
+            }
+            else map.put(name, map.get(name) + 1);
+        }
+
+        for(String name: completion) {
+            Integer cnt = map.get(name);
+            map.put(name, cnt - 1);
+            if(map.get(name) == 0) map.remove(name);
+        }
+        for(String name: map.keySet()) {
+            answer = name;
+        }
+        return answer;
+    }
 }
