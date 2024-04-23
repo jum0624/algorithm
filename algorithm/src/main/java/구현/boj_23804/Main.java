652acc2ead94f1e3adedc630094a0f54faadbcb2
jum0624 <jum0624@naver.com>
2024-04-23 21:52:47 +0900

    add: 구현 기초 문제 풀이

---
+++ algorithm/src/main/java/구현/boj_23804/Main.java
@@ -0,0 +1,37 @@
+package 구현.boj_23804;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        String[][] map = new String[n * 5][n * 5];
+
+        for (int i = 0; i < n * 5; i++) {
+            for (int j = 0; j < n * 5; j++) {
+                if (i < n) {
+                    map[i][j] = "@";
+                }
+                if (i >= (n * 5) - n) {
+                    map[i][j] = "@";
+                }
+
+                if (j < n) {
+                    map[i][j] = "@";
+                }
+
+            }
+        }
+
+        for (int i = 0; i < n * 5; i++) {
+            for (int j = 0; j < n * 5; j++) {
+                if (map[i][j] != null) {
+                    System.out.print(map[i][j]);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
